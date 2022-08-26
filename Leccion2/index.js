54e1b11d48d42a0f21e3b9febbc04715bdfcfcfd
ulra <ra.do.nu20@gmail.com>
2022-08-26 15:03:06 -0400

    leccion2

---
+++ Leccion2/index.js
@@ -0,0 +1,11 @@
+const nombre = "Raul Nuñez";
+let edad = 27;
+const desarrolador = true;
+const nacimiento = new Date("may 06 1995");
+const libro = {
+    titulo: "El alquimista",
+    autor: "Paulo Coelho",
+    fecha: fecha.getFullYear("1988"),
+    url: "http://www.om3.org/upload/spain/paulocoelho_elalquimista.pdf"
+
+}
