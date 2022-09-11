f98187f48000e379f9322cb6d18745ca3e1ebe9e
ulra <ra.do.nu20@gmail.com>
2022-09-10 23:05:29 -0400

    edad

---
+++ Leccion7/edad.js
@@ -0,0 +1,7 @@
+/*- Abre una nueva ventana "about:blank" en Google Chrome
+
+- Abre la consola de desarrollador de Google Chrome (F12)
+
+- Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad*/
+
+const edad = prompt("cual es tu edad")
