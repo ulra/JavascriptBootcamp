ffa9e69ecf76e50e9e561cf15bd2c7ccb15458e9
ulra <ra.do.nu20@gmail.com>
2022-09-12 20:53:57 -0400

    leccion10

---
+++ Leccion10/controller.js
@@ -0,0 +1,8 @@
+export function suma(a,b){
+    return a + b
+}
+
+export function multiplicacion(a,b){
+    return a*b
+}
+
---
+++ Leccion10/index.js
@@ -0,0 +1,6 @@
+import * as matematicas from "./controller.js";
+import chalk from 'chalk';
+
+console.log(matematicas.multiplicacion(1, 2))
+console.log(chalk.green(matematicas.suma(4, 5)))
+
